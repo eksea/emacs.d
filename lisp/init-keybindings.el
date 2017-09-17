f956d29d1ca24bc182b74e728d51d0df7e134920
Jack <eksea@outlook.com>
2017-09-17 13:56:52 +0800

    improve keybindings for company mode

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -17,4 +17,10 @@
 
 (global-set-key (kbd "<f5>") 'geiser-mode-switch-to-repl-and-enter)
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-keybindings)
