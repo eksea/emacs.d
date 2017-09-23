7a8bb4e1e46e1d7bf05387a39f5a45d6ed94eee2
Jack <eksea@outlook.com>
2017-09-23 20:24:24 +0800

    modify neo

--- lisp/custom.el
+++ lisp/custom.el
@@ -7,6 +7,8 @@
  '(company-minimum-prefix-length 1)
  '(evil-leader/leader "SPC")
  '(evil-want-C-u-scroll t)
+ '(neo-autorefresh t)
+ '(neo-window-fixed-size nil)
  '(package-selected-packages
    (quote
     (exec-path-from-shell all-the-icons which-key geiser evil-nerd-commenter evil-surround window-numbering company monokai-theme go-mode racket-mode darkokai-theme swiper counsel smartparens neotree nodejs-repl js2-mode popwin helm-ag web-mode expand-region evil evil-leader))))
