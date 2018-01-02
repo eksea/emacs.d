8375e7441be4f70a607655b507bb7829f2c8fce8
Jack <eksea@outlook.com>
2018-01-02 15:07:27 +0800

    config whitespace style

--- lisp/custom.el
+++ lisp/custom.el
@@ -15,10 +15,11 @@
     (imenu-anywhere moe-theme company-irony irony qml-mode spaceline ggtags htmlize json-mode exec-path-from-shell all-the-icons which-key geiser evil-nerd-commenter evil-surround window-numbering company monokai-theme go-mode racket-mode darkokai-theme swiper counsel smartparens neotree nodejs-repl js2-mode popwin helm-ag web-mode expand-region evil evil-leader)))
  '(whitespace-style
    (quote
-    (face trailing tabs spaces lines newline empty space-after-tab space-before-tab space-mark tab-mark))))
+    (face trailing tabs spaces lines newline empty space-after-tab space-before-tab space-mark))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(whitespace-space ((t (:foreground "dim gray"))))
+ '(whitespace-tab ((t (:foreground "dim gray" :strike-through t)))))
