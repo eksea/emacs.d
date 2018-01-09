6f26fc598e5509c3e7bbb537d853fde74c971ccd
Jack <eksea@outlook.com>
2018-01-09 14:36:53 +0800

    config whitespace

--- lisp/custom.el
+++ lisp/custom.el
@@ -13,9 +13,7 @@
  '(package-selected-packages
    (quote
     (imenu-anywhere moe-theme company-irony irony qml-mode spaceline ggtags htmlize json-mode exec-path-from-shell all-the-icons which-key geiser evil-nerd-commenter evil-surround window-numbering company monokai-theme go-mode racket-mode darkokai-theme swiper counsel smartparens neotree nodejs-repl js2-mode popwin helm-ag web-mode expand-region evil evil-leader)))
- '(whitespace-style
-   (quote
-    (face trailing tabs spaces lines newline empty space-mark))))
+ '(whitespace-style (quote (face tabs space-mark))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
