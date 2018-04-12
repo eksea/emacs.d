0b6169f63a32559cafcd7d0bd37cff02f6e241f4
eksea <eksea@outlook.com>
2018-04-12 14:37:39 +0800

    add doom themes

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -17,24 +17,38 @@
 
 (global-hl-line-mode t)
 
-;; customize font-size
-(set-face-attribute 'default nil :height 140)
+(defun eksea-set-font()
+  (interactive)
+  ;; customize font-size
+  (set-face-attribute 'default nil :height 160)
 
-;; customize font-family
-;; (set-default-font "Anonymous Pro")
-;; Setting English Font
-(set-face-attribute
- 'default nil :font "Inconsolata")
-;; Setting Chinese Font
-(dolist (charset '(kana han symbol cjk-misc bopomofo))
-  (set-fontset-font (frame-parameter nil 'font)
-		    charset
-		    (font-spec :family "SimHei")))
+  ;; customize font-family
+  ;; (set-default-font "Anonymous Pro")
+  ;; Setting English Font
+  (set-face-attribute
+   'default nil :font "Inconsolata")
+  ;; Setting Chinese Font
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font)
+                      charset
+                      (font-spec :family "SimHei")))
+  )
+
+(if window-system
+    (eksea-set-font))
+
 
 ;;(load-theme 'monokai t)
 ;; (load-theme 'darkokai t)
-(require 'moe-theme)
-(moe-dark)
-(moe-theme-set-color 'cyan)
+;;(require 'moe-theme)
+;;(moe-dark)
+;;(moe-theme-set-color 'cyan)
+(require 'doom-themes)
+(load-theme 'doom-one t)
+(doom-themes-visual-bell-config)
+(doom-themes-neotree-config)
+(doom-themes-org-config)
+
+(set-face-foreground 'font-lock-comment-face "#14B696")
 
 (provide 'init-ui)
